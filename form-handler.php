f8685257850d5e8a46ebfe87abc3913de265fed3
Wasikur <69371416+Wasikur@users.noreply.github.com>
2023-08-16 23:40:46 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'contact@domain.name';
+
+$emai_subject = 'New form Submission';
+
+$email_body = "User Name : $name.\n".
+              "User Email : $visitor_email.\n".
+              "Subject : $subject.\n".
+              "User Message : $message.\n";
+
+$to = 'rtwf@rhinotechwastefoundation.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
